1762e0928431626e4b352daa2ed2e26a1e4a25a7
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-03-31 15:09:07 +0900

    restcontroller argument resolver impl

--- src/main/java/com/inspire12/likelionbackend/module/mvc/config/OrderArgumentResolver.java
+++ src/main/java/com/inspire12/likelionbackend/module/mvc/config/OrderArgumentResolver.java
@@ -1,5 +1,6 @@
 package com.inspire12.likelionbackend.module.mvc.config;
 
+import com.inspire12.likelionbackend.module.mvc.model.Customer;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -10,13 +11,23 @@ public class OrderArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         // TODO
-        return true;
-        //        return parameter.getParameterType().equals(Customer.class);
+        // Customer 클래스에 해당 파라미터 존재여부 체크
+//        return true;
+        return parameter.getParameterType().equals(Customer.class);
     }
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 //       TODO
-        throw new UnsupportedOperationException();
+        // 파라미터가 있는지 체크해서, 있으면 Customer 객체 생성해서 리턴
+        // 없으면 없는대로 리턴
+        String customerId = webRequest.getHeader("user-id");
+        if(customerId == null) {
+            return null;
+        }
+        Long customerId_long = Long.parseLong(customerId);
+
+        return new Customer(customerId_long);
+//        throw new UnsupportedOperationException();
     }
 }
