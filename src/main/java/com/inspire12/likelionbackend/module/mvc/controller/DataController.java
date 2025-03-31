80498ee8a5e93ac34d6e537a9a1928705fe3a3af
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-03-31 15:54:41 +0900

    resource get mapping implementing

--- src/main/java/com/inspire12/likelionbackend/module/mvc/controller/DataController.java
+++ src/main/java/com/inspire12/likelionbackend/module/mvc/controller/DataController.java
@@ -1,8 +1,14 @@
 package com.inspire12.likelionbackend.module.mvc.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class DataController { // 뜬금 질문, 제가 왜 클래스명을 RestController 로 만들었을까요?
+    @GetMapping("/resource/load")
+    public void getresourece() {
+        return;
+    }
 }
