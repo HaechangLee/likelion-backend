4cbd93dd2bb4b949c00d3a214eee315b81b95f62
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-04-03 11:33:12 +0900

    unit test practice

--- src/test/java/com/inspire12/likelionbackend/module/order/domain/OrderTest.java
+++ src/test/java/com/inspire12/likelionbackend/module/order/domain/OrderTest.java
@@ -1,14 +1,21 @@
 package com.inspire12.likelionbackend.module.order.domain;
 
+import com.inspire12.likelionbackend.module.order.enums.OrderStatus;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderTest {
 
     @Test
     void 우선_실패하는테스트_만들기() {
         // TODO junit/assertj 를 통해 실패하게 만들기
+        Order testOrder = new Order(1L, 123L, 7L,"abcd123",10000, OrderStatus.ORDERED, LocalDateTime.now());
+        Order result = testOrder.changeAmount(20000);
+        assertThat(result.getTotalAmount()).isEqualTo(20000);
+
     }
 
 }
